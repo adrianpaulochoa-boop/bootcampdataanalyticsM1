7669958ad9d9d8b3f12f96a5a6c124a970f9dd86
Adrian Paúl Ochoa <adrianpaulochoa@gmail.com>
2026-08-21 16:05:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,10 @@ elif modulos == "Funciones":
     caudal = caudal_max*(1 - 0.2*relacion_presion - 0.8*(relacion_presion**2))
     return round(caudal, decimales)
   
-  caudal_maximo = st.number_input("Ingrese el caudal maximo")
-  presion_yacimiento = st.number_input("Ingrese la presion de yacimiento")
-  presion_fondo = st.number_input("Ingrese la presion de fondo fluyente")
-  decimales = st.slider("Seleccione la cantidad de decimales para su resultado")
+  caudal_maximo = st.number_input("Ingrese el caudal maximo", min_value = 0, max_value = 5000, value = 1200)
+  presion_yacimiento = st.number_input("Ingrese la presion de yacimiento", min_value = 0, max_value = 9000, value = 3000)
+  presion_fondo = st.number_input("Ingrese la presion de fondo fluyente", min_value = 0, max_value = 9000, value = 1500)
+  decimales = st.slider("Seleccione la cantidad de decimales para su resultado", min_value = 0, max_value = 4, value = 2)
+
+
 
